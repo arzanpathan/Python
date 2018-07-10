758b48fa6ffc7869d6f8d0dc4310c2a6b08bfece
Arzan Arif Pathan <arzan.pathan8@gmail.com>
2018-07-11 00:08:30 +0530

    Add files via upload

---
+++ Program 4.py
@@ -0,0 +1,4 @@
+#WAP to take 10 inputs from user and print the largest,smallest,total,average(Total/2) out of it
+print('WAP to take 10 inputs from user and print the largest,smallest,total,average(Total/2) out of it')
+for i in range(10):
+ n1=int(input('Enter the Value: '))
