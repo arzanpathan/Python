2a79fd97222b3692228086ace2fde587c33f56a0
Arzan Arif Pathan <arzan.pathan8@gmail.com>
2018-07-12 15:58:52 +0530

    Update Program 4.py

--- Program 4.py
+++ Program 4.py
@@ -1,4 +1,23 @@
 #WAP to take 10 inputs from user and print the largest,smallest,total,average(Total/2) out of it
 print('WAP to take 10 inputs from user and print the largest,smallest,total,average(Total/2) out of it')
+c,b,Total=5,0,0
 for i in range(10):
- n1=int(input('Enter the Value: '))
+ a=int(input('Enter a New No'))
+ if(a>b):
+     b=a
+ else:
+     b=b
+ if(a<c):
+     c=a
+ else:
+     c=c
+ if(a==b):
+     b=a
+ if(a==c):
+     c=a
+ Total=a+Total
+     
+print('Largest Number is: ',b)
+print('Smallest Number is: ',c)
+print('Sum of all Numbers is: ',Total)
+print('Average Number is: ',Total/2)
